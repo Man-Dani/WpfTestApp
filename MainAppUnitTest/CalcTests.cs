1c114dc062480abcd0fa0c0fc2d911daf676e27b
mantay <daniel.mantay@trsystems.de>
2018-01-02 16:40:13 +0100

    Test korrigiert

--- MainAppUnitTest/CalcTests.cs
+++ MainAppUnitTest/CalcTests.cs
@@ -4,21 +4,21 @@ using WpfTestApp;
 
 namespace MainAppUnitTest
 {
-    [TestClass]
-    public class CalcTests
-    {
-        [DataTestMethod]
-        [DataRow(3, 2, 5)]
-        public void TestsSummationOfTwoIntegers(int i1, int i2, int erg)
-        {
-            Assert.AreEqual(erg, Calc.Add(i1, i2));
-        }
+	[TestClass]
+	public class CalcTests
+	{
+		[DataTestMethod]
+		[DataRow(3, 2, 5)]
+		public void TestsSummationOfTwoIntegers(int i1, int i2, int erg)
+		{
+			Assert.AreEqual(erg, Calc.Add(i1, i2));
+		}
 
-        [TestMethod]
-        public void TestsSummationOfTwoIntegers2()
-        {
-            Assert.AreEqual(5, Calc.Add(2, 3));
-        }
-
-    }
+		[TestMethod]
+		public void TestsSummationOfTwoIntegers2()
+		{
+			var sum = Calc.Add(2, 3);
+			Assert.AreEqual(5, sum);
+		}
+	}
 }
