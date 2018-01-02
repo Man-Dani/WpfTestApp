7fa0885d4f1ad776135898a7520d3bdf90226668
mantay <daniel.mantay@trsystems.de>
2018-01-02 13:28:03 +0100

    Rechner hinzugefügt

---
+++ WpfTestApp/Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTestApp
+{
+    public static class Calc
+    {
+
+        #region Methods
+
+        public static int Add(int i1, int i2)
+        {
+            return i1 + i2;
+        }
+
+        #endregion Methods
+
+    }
+}
