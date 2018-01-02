5be463e160d7e100c6466de3514a70155fc08c9e
mantay <daniel.mantay@trsystems.de>
2018-01-02 13:36:01 +0100

    Unit Test hinzugefügt

---
+++ MainAppUnitTest/CalcTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WpfTestApp;
+
+namespace MainAppUnitTest
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [DataTestMethod]
+        [DataRow(3, 2, 5)]
+        public void TestsSummationOfTwoIntegers(int i1, int i2, int erg)
+        {
+            Assert.AreEqual(erg, Calc.Add(i1, i2));
+        }
+    }
+}
---
+++ MainAppUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MainAppUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MainAppUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30023b45-ce4e-40fd-98ff-1476827ba737")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
